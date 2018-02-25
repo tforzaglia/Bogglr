72d540e19c6be52c962770b10fa570548c7828b1
Tom Forzaglia <thomas.forzaglia@gmail.com>
2018-02-25 14:07:38 -0500

    board tiles can be a private property

--- Bogglr/Bogglr/Board/Board.swift
+++ Bogglr/Bogglr/Board/Board.swift
@@ -15,7 +15,7 @@ final class Board {
     let numberOfColumns: Int
 
     /// Array of each tile that makes up the Boggle board
-    private(set) var tiles = [BoardTile]()
+    private var tiles = [BoardTile]()
 
     init(letters: [[Character]]) {
         self.numberOfRows = letters.count
